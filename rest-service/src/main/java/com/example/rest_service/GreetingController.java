3489c0f7404243c11475daec097319c4bbfa984e
Krishal-Modi1920 <krishalmodi171@outlook.com>
2025-01-16 17:24:17 +0530

    Add files via upload

---
+++ rest-service/src/main/java/com/example/rest_service/GreetingController.java
@@ -0,0 +1,20 @@
+// It defines that how someone interacts with our API
+package com.example.rest_service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.atomic.AtomicLong;
+
+// RestController defines a class that is going to perform request mapping and also entry point in our application
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+
+    // greeting?name=Dan
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
